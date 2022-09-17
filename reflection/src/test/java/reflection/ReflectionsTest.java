38b919176069260a3367648ec82985d41dfadd0b
YAHO <69156709+pup-paw@users.noreply.github.com>
2022-09-18 05:22:29 +0900

    Reflection (#4)
    * feat: 리플렉션 학습 테스트
    
    * docs: 리플렉션 관련 공부 정리

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,8 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -10,9 +13,16 @@ class ReflectionsTest {
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        reflections.getTypesAnnotatedWith(Controller.class)
+                .forEach(clazz -> log.info("@Controller - " + clazz.getCanonicalName()));
+        reflections.getTypesAnnotatedWith(Service.class)
+                .forEach(clazz -> log.info("@Service - " + clazz.getCanonicalName()));
+        reflections.getTypesAnnotatedWith(Repository.class)
+                .forEach(clazz -> log.info("@Repository - " + clazz.getCanonicalName()));
+
     }
 }

e9d419740ee23a1a6abedeed2f48488b837aa268
YAHO <69156709+pup-paw@users.noreply.github.com>
2022-10-03 00:03:59 +0900

    DI (#5)
    * feat: 빈을 인스턴스화하고 필드를 주입해 반환하는 기능 구현
    
    * feat: 어노테이션을 기반으로 빈을 인스턴스화하고 필드를 주입해 반환하는 기능 구현

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,11 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +15,56 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(this::initiateBean)
+                .collect(Collectors.toSet());
+        this.beans.forEach(bean -> setFields(bean.getClass(), bean));
+    }
+
+    private Object initiateBean(final Class<?> aClass) {
+        final Constructor<?> constructor;
+        try {
+            constructor = aClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (NoSuchMethodException | InvocationTargetException |
+                 InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setFields(final Class<?> aClass, final Object bean) {
+        for (Field field : aClass.getDeclaredFields()) {
+            try {
+                setField(bean, field);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void setField(Object bean, Field field) throws IllegalAccessException {
+        if (field.isAnnotationPresent(Inject.class)) {
+            Optional<Object> object = beans.stream()
+                    .filter(obj -> field.getType().isAssignableFrom(obj.getClass()))
+                    .findFirst();
+            field.setAccessible(true);
+            if (object.isPresent()) {
+                field.set(bean, object.get());
+            }
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> bean.getClass().equals(aClass))
+                .findFirst()
+                .orElseThrow();
     }
 }
